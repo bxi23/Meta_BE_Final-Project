b7281df717110c960ba2e74043400a80f187c919
Bronson Windows Desktop <iannob21@gmail.com>
2024-08-14 17:27:27 -0400

    Set up mysql database

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', #allows for staticfiles
     'restaurant',
 ]
 
@@ -75,10 +75,23 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    #Mysql databases
+     'default': {
+     'ENGINE': 'django.db.backends.mysql',
+     'NAME': 'LittleLemon',
+     'USER': 'root',
+     'PASSWORD': 'Brindle$1325',
+     'HOST': '127.0.0.1',
+     'PORT': '3306',
+     'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
     }
+ }
 }
 
 
@@ -118,6 +131,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#added a staticfiles dir so they are not tied to a given app
 STATICFILES_DIRS =[
     BASE_DIR / "static",
 ]
